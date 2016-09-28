e8f13f69a3dad39c2a533a283de4028e9451c39f
tzits <tzitsman@gmail.com>
2016-09-28 15:09:43 -0700

    created some routes

--- server.js
+++ server.js
@@ -22,13 +22,53 @@ app.get('/profile', function profilePage(req, res) {
 
 //JSON API endpoints
 
-GET
+// Get All Pets
+app.get('/api/pets', function index(req, res) {
+	db.Pet.find({}, function(err, allPets) {
+		if (err) {throw err;};
+		res.json(allPets);
+	});
+});
 
-POST
 
-UPDATE
+// Get One Pet
+app.get('/api/pets/:id', function show(req, res) {
+	var petId = req.params.id
+	db.Pet.findById(petId, function(err, pet) {
+		if (err) {throw err;};
+		res.json(pet);
+	});
+});
 
-DELETE
+
+// Create pet
+app.post('/api/pets', function create(req, res) {
+	db.Pet.create(req.body, function(err, pet) {
+		if (err) {throw err;};
+		res.json(pet);
+	});
+});
+
+// Delete pet
+app.delete('/api/pets/:id', function destroy(req,res) {
+	var petId = req.params.id;
+	db.Pet.findByIdAndRemove(petId, function(err,foundPet) {
+		if (err) { throw err; };
+		res.json(foundPet + " is gone forever :(");
+	});
+});
+
+// Update pet
+app.put('/api/pets/:id', function update(req,res) {
+	var updatedPet = req.body
+	var petId = req.params.id;
+	db.Pet.findOneAndUpdate(petId, updatedPet, function(err,foundPet) {
+		if (err) {throw err};
+		foundPet.save();
+		res.json(foundPet);
+	});
+});
+
 
 
 //server
